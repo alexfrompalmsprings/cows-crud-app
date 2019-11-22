25869510d1f18b54c3ed5fb98bae6c5b097f47bf
AlexAvila <alexavila.oc@gmail.com>
2019-11-21 21:38:14 -0800

    modify webpack config & server runs

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 // express & port
 const express = require('express');
 const app = express();
-const port = 3004;
-const path = require('path');
+const port = 3005;
+// const path = require('path');
 
 // connects the db
 const db = require("./db");
@@ -16,6 +16,11 @@ app.use(express.json())
 app.use(express.urlencoded({
   extended: true
 }))
+
+app.use(express.static(__dirname));
+console.log(__dirname);
+app.use(express.static(path.join(__dirname, 'dist')));
+
 
 
 //! need to add my db queries into here!
@@ -43,16 +48,21 @@ app.post('/api/cows', (req, res) => {
   // var cowDescription = req.body.description;
 
   console.log('---->', req.body);
-  db.query(queryPost, insertVals, function (err, results) {
-    if (err) {
-      console.log('#################',err)
-      res.sendStatus(500)
-    } else {
-      console.log('results ------->', results)
-      // res.sendStatus(200)
-      res.json({cowName, cowDescription});
-    }
-  })
+  try{
+    db.query(queryPost, insertVals, function (err, results) {
+      if (err) {
+        console.log('#################',err)
+        res.status(500).json({err})
+      } else {
+        console.log('results ------->', results)
+        // res.sendStatus(200)
+        res.json({cowName: req.body.cowName, cowDescription: req.body.cowDescription});
+      }
+    })
+
+  } catch(err) {
+    res.status(500).json({err})
+  }
 })
 
 
